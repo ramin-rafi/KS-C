ba0cd821958c5914d4478e01477d8d806f50b592
SYSTEMS\ramin.rafi <ramin.rafi@systemsltd.com>
2022-03-28 15:21:10 +0500

    Lint errors removed

--- KickStarterPackage-Custom/cartridges/kickstarterpackage/cartridge/experience/components/layouts/teaserLayout.js
+++ KickStarterPackage-Custom/cartridges/kickstarterpackage/cartridge/experience/components/layouts/teaserLayout.js
@@ -5,48 +5,48 @@ var HashMap = require('dw/util/HashMap');
 var PageRenderHelper = require('~/cartridge/experience/utilities/PageRenderHelper.js');
 
 module.exports.render = function (context) {
-    var model = new HashMap();
-    var component = context.component;
-    var content = context.content;
+  var model = new HashMap();
+  var component = context.component;
+  var content = context.content;
 
-    // automatically register configured regions
-    model.regions = PageRenderHelper.getRegionModelRegistry(component);
+  // automatically register configured regions
+  model.regions = PageRenderHelper.getRegionModelRegistry(component);
 
-    if (content.addClass) {
-        model.addClass = content.addClass;
-    }
+  if (content.addClass) {
+    model.addClass = content.addClass;
+  }
 
-    if (content.teaserLayoutConfig) {
-        model.teaserLayoutConfig = content.teaserLayoutConfig;
-    }
+  if (content.teaserLayoutConfig) {
+    model.teaserLayoutConfig = content.teaserLayoutConfig;
+  }
 
-    if (content.teaserLayoutPadY) {
-        model.teaserLayoutPadY = content.teaserLayoutPadY;
-    }
+  if (content.teaserLayoutPadY) {
+    model.teaserLayoutPadY = content.teaserLayoutPadY;
+  }
 
-    if (content.teaserLayoutCol) {
-        model.teaserLayoutCol = content.teaserLayoutCol;
-    }
+  if (content.teaserLayoutCol) {
+    model.teaserLayoutCol = content.teaserLayoutCol;
+  }
 
-    if (content.teaserLayoutHeading) {
-        model.teaserLayoutHeading = content.teaserLayoutHeading;
-    }
+  if (content.teaserLayoutHeading) {
+    model.teaserLayoutHeading = content.teaserLayoutHeading;
+  }
 
-    if (content.teaserHeadingAlignment) {
-        model.teaserHeadingAlignment = content.teaserHeadingAlignment;
-    }
+  if (content.teaserHeadingAlignment) {
+    model.teaserHeadingAlignment = content.teaserHeadingAlignment;
+  }
 
-    if (content.teaserLayoutMagazine) {
-        model.teaserLayoutMagazine = content.teaserLayoutMagazine;
-    }
+  if (content.teaserLayoutMagazine) {
+    model.teaserLayoutMagazine = content.teaserLayoutMagazine;
+  }
 
-    if (content.teaserLayoutMagazineName) {
-        model.teaserLayoutMagazineName = content.teaserLayoutMagazineName;
-    }
+  if (content.teaserLayoutMagazineName) {
+    model.teaserLayoutMagazineName = content.teaserLayoutMagazineName;
+  }
 
-    if (content.teaserLayoutMagazineDisclaimer) {
-        model.teaserLayoutMagazineDisclaimer = content.teaserLayoutMagazineDisclaimer;
-    }
+  if (content.teaserLayoutMagazineDisclaimer) {
+    model.teaserLayoutMagazineDisclaimer = content.teaserLayoutMagazineDisclaimer;
+  }
 
-    return new Template('experience/components/layouts/teaserLayout').render(model).text;
+  return new Template('experience/components/layouts/teaserLayout').render(model).text;
 };
