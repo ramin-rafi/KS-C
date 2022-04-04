db2065f961f4dec865366de0e76782fd836fe0ed
SYSTEMS\ramin.rafi <ramin.rafi@systemsltd.com>
2022-04-04 21:24:53 +0500

    Customized logo added

--- KickStarterPackage-Custom/cartridges/kickstarterpackage/cartridge/client/default/js/main.js
+++ KickStarterPackage-Custom/cartridges/kickstarterpackage/cartridge/client/default/js/main.js
@@ -47,3 +47,80 @@ $('.card-slider').slick({
     }
   }]
 });
+
+$(document).ready(function () {
+  if ($('.banner-slider .slick-active .caption').hasClass('white')) {
+      $('.banner-slider').addClass('white-caption');
+  } else {
+      $('.banner-slider').removeClass('white-caption');
+  }
+  $('.slick-dots li').on('click', function () {
+      var vidID = $(this).find('button').attr('id');
+      $('iframe').map(function () {
+          if (
+              vidID &&
+              vidID ===
+                  $(this)
+                      .parents('.slick-slide:not(".slick-cloned")')
+                      .attr('aria-describedby')
+          ) {
+              return false;
+          } else { // eslint-disable-line no-else-return
+              $f(this).api('pause');
+              return true;
+          }
+      });
+  });
+
+  $('.slick-arrow').on('click', function (e) {
+      $('iframe').map(function () {
+          $f(this).api('pause');
+          return true;
+      });
+      e.preventDefault();
+  });
+
+  if (('#banVideo').length > 0) {
+      var video1 = $('#banVideo');
+      video1.currentTime = 0;
+
+      $('.mute-bt').click(function () {
+          $('.unmute-bt').show();
+          $('.mute-bt').hide();
+          $('#banVideo').prop('muted', false);
+      });
+
+      $('.unmute-bt').click(function () {
+          $('.mute-bt').show();
+          $('.unmute-bt').hide();
+          $('#banVideo').prop('muted', true);
+      });
+
+      $('.pause-bt').click(function () {
+          $('.play-bt').show();
+          $('.pause-bt').hide();
+          $('.pause-bt').addClass('active');
+          var banVideo = document.getElementById('banVideo');
+          banVideo.pause();
+      });
+
+      $('.play-bt').click(function () {
+          $('.play-bt').hide();
+          $('.pause-bt').show();
+          var banVideo = document.getElementById('banVideo');
+          banVideo.play();
+      });
+
+      $('.editorial-teaser-slide').on('afterChange', function () {
+          if ($('.slick-current .slide-item video').hasClass('bs-video')) {
+              $('.slick-current video')[0].play();
+          }
+      });
+
+      $('.editorial-teaser-slide').on('beforeChange', function () {
+          if ($('.slide-item video').hasClass('bs-video')) {
+              $('video')[0].pause();
+          }
+      });
+  }
+});
